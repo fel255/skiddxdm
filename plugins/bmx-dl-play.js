73e314ff365efb1302c4fce1dd8ea24c6dc92d80
fel255 <feliciandehecker@gmail.com>
2025-07-04 08:37:42 +0300

    Update bmx-dl-play.js

--- plugins/bmx-dl-play.js
+++ plugins/bmx-dl-play.js
@@ -37,7 +37,7 @@ let handler = async (m, { conn, text, botname }) => {
             m.chat,
             {
               image: { url: songData.thumbnail },
-              caption: `SYLIVANUS THE SKIDDBMX BOT
+              caption: `FELICIAN THE SKIDDBMX BOT
 ╭═════════════════⊷
 ║ 🎶 *Title:* ${songData.title}
 ║ 🎤 *Artist:* ${songData.artist}
@@ -74,7 +74,7 @@ let handler = async (m, { conn, text, botname }) => {
           );
 
           // Send success message
-          await m.reply("✅ *Silva MD – World-class bot just successfully sent you what you requested! 🎶*");
+          await m.reply("✅ *SKIDDXMD – World-class bot just successfully sent you what you requested! 🎶*");
 
           return; // Stop execution if successful
         }
